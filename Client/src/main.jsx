f1abaf62d0b41fe8f9d5294d9bf781d322af4c4b
Omkar Pawar <pawaromkar1654@gmail.com>
2025-01-17 16:07:37 +0530

    1

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from "@/components/ui/sonner"
-import { SocketProvider } from "./context/socketContext.jsx"
+import { SocketProvider } from "./Context/socketContext"
 //build problem here
 
 createRoot(document.getElementById('root')).render(
